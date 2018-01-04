f3829abc24fddacfb4c92c1640c5510c79fe3d2b
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2018-01-04 20:22:30 +0100

    git - Committed at: 2018-01-04 20:22 Uhr

--- git_it.sh
+++ git_it.sh
@@ -14,8 +14,14 @@ git add -A
 # Generate Date for Commit-Text
 now=$(date +"%Y-%m-%d %H:%M Uhr")
 
-# Commit changes
-git commit -m "Edited comments - Committed at: ($now)"
+# Commit changes (If a string was added, use the added string.)
+if [ -z "$1" ]; then
+    #zero length argument 
+    git commit -m "Committed at: $now"
+else
+    #non-zero length
+    git commit -m "$1 - Committed at: $now"
+fi
 
 #git remote add origin git@github.com:mmuyakwa/bash-scripts.git
 
