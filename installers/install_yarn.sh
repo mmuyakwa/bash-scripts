539d057aa349ddd8c9268702a55121ed0c068c69
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2018-01-23 05:28:22 +0100

    Added installer for YARN.

---
+++ installers/install_yarn.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#title:         install_yarn.sh
+#description:   This script installs YARN ("https://yarnpkg.com").
+#author:        Michael Muyakwa
+#created:       2018-01-23
+#updated:       N/A
+#version:       0.6
+#license:       MIT
+#usage:         install_yarn.sh
+#==============================================================================
+
+SUDO=''
+if [ $(id -u) -ne 0 ]; then
+    SUDO='sudo'
+    echo "Your not root."
+    echo "Running apt-get with SUDO."
+fi
+
+$SUDO apt-get update && sudo apt-get install curl -y
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | $SUDO apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | $SUDO tee /etc/apt/sources.list.d/yarn.list
+
+$SUDO apt-get update && sudo apt-get install yarn -y
