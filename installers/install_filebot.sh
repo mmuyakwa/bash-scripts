b7c2229fffab2dd3dd689cb7eb8670422eeda370
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2018-11-28 22:29:01 +0100

    Added Info to the filebot-installer. (Free Legacy-Version) - Committed at: 2018-11-28 22:29 Uhr

--- installers/install_filebot.sh
+++ installers/install_filebot.sh
@@ -12,3 +12,9 @@ apt-get update
 # 4. Install FileBot and its dependencies
 apt-get install apt-transport-https -y
 apt-get install filebot #--install-suggests
+
+
+#####NEWS!
+
+# It's easier to install the legacy-Version, which is free of charge.
+# http://sourceforge.mirrorservice.org/f/project/fi/filebot/filebot/FileBot_4.7.9/filebot_4.7.9_amd64.deb
