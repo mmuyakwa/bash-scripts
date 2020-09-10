f010ffc39fe1264f4f88505a42427797ec3c11f1
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2020-09-10 19:34:17 +0200

    Changed Ansible-install from PPA to entry in sources.list - Committed at: 2020-09-10 19:34 Uhr

--- installers/install_ansible.sh
+++ installers/install_ansible.sh
@@ -9,10 +9,16 @@
 SUDO=''
 [ $(id -u) -ne 0 ] && { SUDO='sudo'; echo "Your not root."; echo "Running commands with SUDO."; }
 
-$SUDO apt-get update -y
-$SUDO apt-get install software-properties-common -y
-$SUDO apt-add-repository --yes --update ppa:ansible/ansible
-$SUDO apt-get update -y
-$SUDO apt-get install ansible -y
-$SUDO sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-$SUDO service ssh restart
+# $SUDO apt-get update -y
+# $SUDO apt-get install software-properties-common -y
+# $SUDO apt-add-repository --yes --update ppa:ansible/ansible
+# $SUDO apt-get update -y
+# $SUDO apt-get install ansible -y
+# $SUDO sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# $SUDO service ssh restart
+
+
+$SUDO echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> etc/apt/sources.list:
+$SUDO apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+$SUDO apt update -y
+$SUDO apt install ansible
