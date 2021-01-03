84b3195174d13a7416efe398d9ebc284b2fb62ed
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2021-01-03 15:56:47 +0100

    Changed Jenkins-Docker - Committed at: 2021-01-03 15:56 Uhr

--- installers/docker/jenkins/install_jenkins_debian.sh
+++ installers/docker/jenkins/install_jenkins_debian.sh
@@ -4,20 +4,29 @@
 
 # install docker
 
-apt -y install software-properties-common dirmngr apt-transport-https lsb-release ca-certificates curl
+# root is always user_id 0
+SUDO=''
+[ "$(id -u)" -ne 0 ] && { SUDO='sudo'; echo "Your not root."; echo "Running commands with SUDO."; }
 
-#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+$SUDO apt -y install software-properties-common dirmngr apt-transport-https lsb-release ca-certificates curl
 
-#apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt-get update
-#apt-get install -y docker-ce
-#systemctl enable docker
-#systemctl start docker
+#curl -fsSL https://download.docker.com/linux/debian/gpg | $SUDO apt-key add -
+
+#$SUDO apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+$SUDO apt-get update
+#$SUDO apt-get install -y docker-ce
+#$SUDO systemctl enable docker
+#$SUDO systemctl start docker
 
 # run jenkins
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8780:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+$SUDO mkdir -p /var/jenkins_home
+$SUDO chown -R 1000:1000 /var/jenkins_home/
+
+wget https://github.com/wardviaene/jenkins-docker/raw/master/Dockerfile
+#docker build -t jenkins-docker .
+#docker run -p 8780:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins -d jenkins-docker
+
+docker-compose up -d --build
 
 # show endpoint
 echo 'Jenkins installed'
