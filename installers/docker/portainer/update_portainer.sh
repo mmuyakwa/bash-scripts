a82c43ec4a8bee284895bff6b0787f8905e7ba96
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2021-01-07 01:25:05 +0100

    Added Script to update Portainer - Committed at: 2021-01-07 01:25 Uhr

---
+++ installers/docker/portainer/update_portainer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# root is always user_id 0
+SUDO=''
+[ "$(id -u)" -ne 0 ] && { SUDO='sudo'; echo "Your not root."; echo "Running commands with SUDO."; }
+
+
+$SUDO docker pull portainer/portainer-ce
+$SUDO docker stop portainer
+$SUDO docker rm portainer
+$SUDO docker run --name portainer --restart always -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
