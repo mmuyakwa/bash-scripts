08897bae1a68a0159d10e7b40aa874aeb060ca76
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2021-05-28 01:52:08 +0200

    Added Vultr-subfolder - Committed at: 2021-05-28 01:52 Uhr

---
+++ vultr/update-rdp-firewall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ext_ip=$(wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>')
+echo "My IP: $ext_ip"
+
+vultr-cli firewall rule delete cc1360ad 1
+vultr-cli firewall rule create -c $ext_ip/32 -i cc1360ad -o 3389 -p tcp
+vultr-cli firewall rule list cc1360ad
